b879ff60e71c874f30a338debab58a25dc17b235
bacemkhlifi <bacemkhlifi001@gmail.com>
2021-02-18 02:52:30 +0100

    make it better

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://bacem:khlifi21762608@cluster0.jdx32.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true},(err)=>{
+mongoose.connect(process.env.MONGODB_URL||'mongodb+srv://bacem:khlifi21762608@cluster0.jdx32.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true},(err)=>{
     if(err){
         console.log(err)
     }
